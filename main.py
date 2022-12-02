2569dad892d68dfd6917972e663995033bf53587
Connor Rawls <connor@Connors-MacBook-Pro.local>
2022-12-01 22:06:30 -0600

    tasks

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from Utilities.utilities import *
 from initTables import *
+
+# GUI SHIT
+
+# 1. Provide user ability to load scenario file
+
+# 2. Use file path as input to fromCSV
+
+# 3. Use data fromCSV to insertData(scenario)
+
+# GUI user input for num_wl
+
+# Generate trigger button calls createWorkload(num_wls)
+
+# 4. Display workload tables to user
+    # 5 wls --> workload1, workload2, ..., workload5
